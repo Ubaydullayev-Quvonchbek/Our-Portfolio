2cc21db9949fedd292e4362ea1691c0d9ad5a0d2
PROUZBMASTER <azizbekislomboyev2@gmail.com>
2023-08-07 12:30:06 +0500

    start

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     WindiCSS(),
